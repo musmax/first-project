29d571b9645816c0bbc8f96bebf7133700cea14a
musa ojo <musaojo1710@yahoo.com>
2021-12-07 23:59:54 +0100

    i change this conn

--- conn.php
+++ conn.php
@@ -1,6 +1,16 @@
 <?php
 //session_start();
-$db = mysqli_connect('localhost', 'root', '', 'loginsystem');
+// connection for local srver
+/*$db = mysqli_connect('localhost', 'root', '', 'loginsystem');
+if(mysqli_connect_errno()){
+	echo "Database Connection failed with the following errors:".mysqli_connect_error();
+	die();
+}
+else{
+	//echo "Database Connection was Successful";
+}*/
+// connection for remote server
+$db = mysqli_connect(' remotemysql.com', '2Bew5a62cw', 'nElk632C2g', ' 2Bew5a62cw');
 if(mysqli_connect_errno()){
 	echo "Database Connection failed with the following errors:".mysqli_connect_error();
 	die();
@@ -9,3 +19,5 @@ else{
 	//echo "Database Connection was Successful";
 }
 ?>
+
+
